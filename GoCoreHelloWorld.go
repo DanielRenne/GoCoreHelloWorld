e74ac79ed8e5171efe5671586593953d1619841e
Daniel Renne <Daniel Renne>
2016-06-17 16:07:36 -0400

    Updated Main file.

---
+++ GoCoreHelloWorld.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/DanielRenne/GoCore/core/app"
+	_ "github.com/DanielRenne/GoCoreHelloWorld/webAPIs/v1/webAPI"
+)
+
+func main() {
+	//Run First.
+	app.Initialize("src/github.com/DanielRenne/GoCoreHelloWorld")
+
+	//Add your Application Code here.
+
+	//Run Last.
+	app.Run()
+}
